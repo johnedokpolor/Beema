dbe3649fdc88144af2bf976df80e5d6a37edfce7
John O. Edokpolor <139572462+johnedokpolor@users.noreply.github.com>
2024-10-10 05:18:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import ContextProvider from "./context/context.jsx";
+import ContextProvider from "./context/Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ContextProvider>
